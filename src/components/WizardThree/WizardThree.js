e9f97dfdd03cc7c9a10c039c8c9532cc42014129
Megan Duclos <37349288+megsdev@users.noreply.github.com>
2018-06-21 16:07:29 -0600

    finished

--- src/components/WizardThree/WizardThree.js
+++ src/components/WizardThree/WizardThree.js
@@ -1,5 +1,7 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
+import { updateProp } from '../../ducks/reducer'
 
 class WizardThree extends Component {
     render(){
@@ -8,9 +10,9 @@ class WizardThree extends Component {
                 <div className="vert-align">
                    <p> What property are you looking to use the loan on? </p><br />
                     <div className="row">
-                        <Link to="/wFour"><button value="primaryHome" onClick={this.props.propToBeUsedOn}>Primary Home</button></Link>
-                        <Link to="/wFour"><button value="rentalProperty" onClick={this.props.propToBeUsedOn}>Rental Property</button></Link>
-                        <Link to="/wFour"><button value="secondaryHome" onClick={this.props.propToBeUsedOn}>Secondary Home</button></Link>
+                        <Link to="/wFour"><button value="primaryHome" onClick={ () => updateProp('Primary Home') }>Primary Home</button></Link>
+                        <Link to="/wFour"><button value="rentalProperty" onClick={ () => updateProp('Rental Property') }>Rental Property</button></Link>
+                        <Link to="/wFour"><button value="secondaryHome" onClick={ () => updateProp('Secondary Home') }>Secondary Home</button></Link>
                     </div>
                 </div>           
             </div>
@@ -18,4 +20,12 @@ class WizardThree extends Component {
     }
 }
 
-export default WizardThree; 
+function mapStateToProps(state) {
+    const { propToBeUsedOn } = state
+
+    return {
+        propToBeUsedOn
+    }
+}
+
+export default connect( mapStateToProps, {updateProp})(WizardThree) 
